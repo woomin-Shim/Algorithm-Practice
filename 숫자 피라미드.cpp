f2c052e2e7f3737d6a59a4ba2216bdfe6952a084
Woomin <worejj@gmail.com>
2021-02-02 15:06:04 +0900

    number피라미드

--- 숫자 피라미드.cpp
+++ 숫자 피라미드.cpp
@@ -4,29 +4,45 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int N, S;
-	cin >> N >> S;
-	int arr[100][100];
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 2 * (1 + i) - 1; j++) {
-			arr[i][j] = S++;
-			if (S == 10) S = 1;
-		}
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = i; j < N - 1; j++) {
+	int n, s, num=1;
+	cin >> n >> s;
+	int PreviousFirst, previousLast = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i-1; j++)
 			cout << ' ';
+
+		if (i % 2 == 0) {  //odd
+			int currentFirst;
+
+			if (i == 0) currentFirst = s;
+			else {
+				currentFirst = previousLast;
+
+				for (int j = 0; j < num; j++) {
+					currentFirst += 1;
+					if (currentFirst == 10) currentFirst = 1;
+				}
+			}
+				PreviousFirst = currentFirst;
+			
+			for (int j = 0; j < num; j++) {
+				cout << currentFirst;
+				currentFirst -= 1;
+				if (currentFirst == 0) currentFirst = 9;
+			}
+			num += 2;
 		}
-		if ((i + 1) % 2 == 0) {//Even
-			for (int k = 0; k < 2 * (1 + i) - 1; k++)
-				cout << arr[i][k];
-		}
-		else { //Odd 
-			int cnt = 2 * (i + 1) - 1;
-			for (int z = cnt - 1; z >= 0; z--)
-				cout << arr[i][z];
+		else { //even
+			int currentFirst = PreviousFirst + 1;
+			if (currentFirst == 10) currentFirst = 1;
+
+			for (int j = 0; j < num; j++) {
+				cout << currentFirst;
+				previousLast = currentFirst;
+				currentFirst += 1;
+				if (currentFirst == 10) currentFirst = 1;					
+			}
+			num += 2;
 		}
 		cout << "\n";
 	}
