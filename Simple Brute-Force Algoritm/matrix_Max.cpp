e9b65c7f41848e4c9f5570366280c3a8a98ad943
Woomin <worejj@gmail.com>
2021-02-04 06:13:40 +0900

    matrix

--- Simple Brute-Force Algoritm/matrix_Max.cpp
+++ Simple Brute-Force Algoritm/matrix_Max.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 /*행렬에 쓰여진 81개의 자연수가 주어질 때 
   이들 중 최대값을 찾고 몇행 몇열인지 출력 
-  최대값이 여러값일 경우 행열이 가장 작은 곳으로 출력*/
+  최대값이 여러값일 경우 행이 가장 작은 곳으로 출력
+  행 번호도 같을시 열이 가장 작은 곳으로 출력*/
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -19,26 +20,20 @@ int main() {
 		for (int j = 0; j < 9; j++) {
 			if (arr[i][j] > big) {
 				big = arr[i][j]; //Max
+				ry = i;
+				rx = j;
+			}
+			else if (arr[i][j] == big) {  
+				if (i < ry)
+					ry = i;
+				else if(i == ry) {
+					if (j < rx)
+						rx = j;
+				}
 			}
 		}
-	}
-	int mat_i[9] = { 9 }, mat_j[9] = { 9 };
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			if (arr[i][j]==big) {
-				mat_i[i] = i;
-				mat_j[j] = j;
-			}
-		}
-	}
-	int min_i = 100, min_j = 100;
-	for (int i = 0; i < 9; i++) {
-		if (min_i > mat_i[i])
-			min_i = mat_i[i];
-		if (min_j > mat_j[i])
-			min_j = mat_j[i];
 	}
 	cout << big << "\n";
-	cout << min_i+1 << ' ' << min_j+1;
+	cout << ry+1 << ' ' << rx+1;
 
 }
