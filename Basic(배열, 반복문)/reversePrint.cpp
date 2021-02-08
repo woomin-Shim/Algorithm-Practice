0efa4a20f27227b044e64ff4d0957a882e1f83f1
Woomin <worejj@gmail.com>
2021-02-08 17:30:04 +0900

    거꾸로 출력

---
+++ Basic(배열, 반복문)/reversePrint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = n-1; i>=0; i--)
+		cout << arr[i] << ' ';
+}
