e534566ea745ecc3a633a998c4fb8ae0ea899e35
Woomin <worejj@gmail.com>
2021-02-16 13:36:10 +0900

    k번째큰수

--- Basic Sort/k번째큰수.cpp
+++ Basic Sort/k번째큰수.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 using namespace std;
 
-/*첫째 줄에 자연수 N, K가 주어지고 (1<=N<=100,000, 1<=K<=10)
-  두번째 줄에 N개의 자연수가 주어진다.
-  K번째로 큰 수를 출력. 단, k가 1이라면 가장 큰수를 출력. */
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n, k;
+	int data[100000] = { 0 };
 
 	cin >> n >> k;
-	int* data = new int[n];
 	for (int i = 0; i < n; i++)
 		cin >> data[i];
-	
-	int cnt = 0, result=0;
-	for (int i = 0; i < n; i++) {
-		cnt++;
-		for (int j = 0; j < n - i - 1; j++) {
-			if (data[j] > data[j + 1]) {
-				int temp = data[j];
-				data[j] = data[j + 1];
-				data[j + 1] = temp;
-			}
-			if (cnt == k)
-				result = data[n - i-1];
-			
+
+	//선택 정렬(내림 차순)
+	for (int i = 0; i < k; i++) {
+		int idx = i;
+		for (int j = i; j < n; j++) {
+			if (data[idx] < data[j])
+				idx = j;
 		}
+		int temp = data[i];
+		data[i] = data[idx];
+		data[idx] = temp;
 	}
-	
-		cout << result << ' ';
 
-
+	cout << data[k - 1];
 
 }
