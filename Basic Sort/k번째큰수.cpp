fb817252f6fa483f32d85adc71cf9626721857a4
Woomin <worejj@gmail.com>
2021-02-16 13:51:03 +0900

    k번째큰수

--- Basic Sort/k번째큰수.cpp
+++ Basic Sort/k번째큰수.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int arr[100000];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n, k;
-	int data[100000] = { 0 };
 
 	cin >> n >> k;
 	for (int i = 0; i < n; i++)
-		cin >> data[i];
+		cin >> arr[i];
 
 	//선택 정렬(내림 차순)
 	for (int i = 0; i < k; i++) {
 		int idx = i;
 		for (int j = i; j < n; j++) {
-			if (data[idx] < data[j])
+			if (arr[idx] < arr[j])
 				idx = j;
 		}
-		int temp = data[i];
-		data[i] = data[idx];
-		data[idx] = temp;
+		int temp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = temp;
 	}
 
-	cout << data[k - 1];
+	cout << arr[k];
 
 }
