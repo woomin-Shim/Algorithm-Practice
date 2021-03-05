c5dd804e2fa4bc0a9a87e66f6a015b6c17836905
Woomin <worejj@gmail.com>
2021-03-05 20:44:22 +0900

    permutation

--- Recursive Function/순열.cpp
+++ Recursive Function/순열.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
+int n, r;
 char alpha[100];
 bool TF[100];
 
-void getResult(int x, int n, int r, char alpha[]) {  //x번째 반복문 
+void getResult(int x) {
     if (x >= r) {
-        cout << alpha;
-        cout << '\n';
+        cout << alpha << '\n';
     }
     else {
         for (int i = 0; i < n; i++) {
+            char c = i + 'a';
             if (TF[i] == false) {
-                char c = i + 'a';
                 alpha[x] = c;
                 TF[i] = true;
 
-                getResult(x + 1, n, r, alpha);
+                getResult(x + 1);
                 TF[i] = false;
                 alpha[x] = 0;
             }
@@ -24,7 +24,6 @@ void getResult(int x, int n, int r, char alpha[]) {  //x번째 반복문
 }
 
 int main() {
-    int n, r;
     cin >> n >> r;
-    getResult(0, n, r, alpha);
+    getResult(0);
 }
