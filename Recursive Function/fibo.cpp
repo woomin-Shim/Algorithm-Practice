62efc853e31743efad05704dd3dd38e2fa95a22c
Woomin <worejj@gmail.com>
2021-04-17 23:39:18 +0900

    fibo

---
+++ Recursive Function/fibo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int fibo(int n) {  //print n'th fibonacci number
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	else {
+		return fibo(n - 1) + fibo(n - 2);
+	}
+}
+
+int main() {
+	int num;
+	cin >> num;
+	cout << fibo(num);
+}
